94aad37d11c6b559840274b56976213f4dbd01f0
davisilvas <morg.daviss@gmail.com>
2024-10-24 20:16:41 -0300

    add form signup frontend

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   resources :articles
   #resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
